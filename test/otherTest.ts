9d96f5871f7df06f7dedbc4a8dda3937cca9edda
Stephen Laughton <stephenlaughton@gmail.com>
2017-10-16 16:11:41 -0700

    a little test of arrays

--- test/otherTest.ts
+++ test/otherTest.ts
@@ -2,10 +2,14 @@ import test from 'ava';
 import { Contact } from './Contact';
 import { seaq } from './../src/Seaq';
 
-
-
 test('getProperty', t => {
   const contacts: Contact[] = require('./contacts.json');
   const searchContacts = seaq(contacts, 'nath ra', ['givenName', 'familyName']);
+  console.log(searchContacts.map(contact => contact.givenName + ' ' + contact.familyName));
+});
+
+test('getProperty', t => {
+  const contacts: Contact[] = require('./contacts.json');
+  const searchContacts = seaq(contacts, 'dwi', ['emailAddresses.email']);
   console.log(searchContacts.map(contact => contact.givenName + ' ' + contact.familyName));
 });
