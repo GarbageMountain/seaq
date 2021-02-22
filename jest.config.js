0093bc5cf7eae02bb36abb3320fec712b9a111de
Benjamin Schachter <benschac@gmail.com>
2021-02-21 23:02:34 -0500

    remove cov folder from git

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,4 @@ const { paths } = require('tsdx/dist/constants');
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 
-// Questions
 module.exports = createJestConfig(undefined, paths.appRoot);
